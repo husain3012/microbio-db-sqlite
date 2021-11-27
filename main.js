4e3a8c54c01b647b7db9e7081e0f3b9a7e3bc9b4
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2021-11-27 14:57:30 +0530

    seamles updae config

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require('electron-updater');
 
 const server = require("./app"); //ADD THIS
 
@@ -37,11 +38,27 @@ app.on("window-all-closed", function () {
   }
 });
 
-// app.on("activate", function () {
-//   if (mainWindow === null) {
-//     createWindow();
-//   }
-// });
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
 
 // try {
 //   require('electron-reloader')(module)
