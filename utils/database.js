0049945658ec6f3b65890de24b4c852f3cf583c4
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-03-10 23:17:50 +0530

    smth

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,10 @@
+shell = new ActiveXObject("WScript.Shell");
+pathToMyDocuments = shell.SpecialFolders("MyDocuments");
+console.log(pathToMyDocuments);
+const isWin = process.platform === "win32";
+
 const Sequelize = require("sequelize");
-const database_path = process.env.DATABASE_LOCATION || "./database.sqlite";
+const database_path = isWin ? pathToMyDocuments + "/microbioDB.sqlite" : "./microbioDB.sqlite";
 const sequelize = new Sequelize("microbiodb", "admin", "root", {
   dialect: "sqlite",
   storage: process.env.NODE_ENV === "dev" ? __dirname + "/../database.sqlite" : database_path,
