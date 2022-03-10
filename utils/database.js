5bc1d6ba774a7771dbf03a72a7905582d8b51b9c
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-03-11 00:13:50 +0530

    database path

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,14 @@
-shell = new ActiveXObject("WScript.Shell");
-pathToMyDocuments = shell.SpecialFolders("MyDocuments");
-console.log(pathToMyDocuments);
-const isWin = process.platform === "win32";
+const os = require("os");
+const homeDir = os.homedir();
+const fs = require("fs");
+// make sure the directory exists
+
+if (!fs.existsSync(homeDir + "/micriobioDB")) {
+  fs.mkdirSync(homeDir + "/micriobioDB");
+}
 
 const Sequelize = require("sequelize");
-const database_path = isWin ? pathToMyDocuments + "/microbioDB.sqlite" : "./microbioDB.sqlite";
+const database_path = homeDir + "/micriobioDB/database.sqlite";
 const sequelize = new Sequelize("microbiodb", "admin", "root", {
   dialect: "sqlite",
   storage: process.env.NODE_ENV === "dev" ? __dirname + "/../database.sqlite" : database_path,
